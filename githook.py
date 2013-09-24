6ccf29a3e0cb7f66293745747280b0918b3d4d1b
Rami Al-Ghanmi <rami@ramialghanmi.com>
2013-09-23 17:00:07 -0700

    added missing imports

--- githook.py
+++ githook.py
@@ -3,6 +3,9 @@ from flask import request
 
 import simplejson
 
+import sys
+from subprocess import CalledProcessError, check_output
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,14 +20,25 @@ This a sample hook
 @app.route('/deploy', methods=['GET', 'POST'])
 def deploy():
     if request.method == 'POST':
-        #app.logger.debug(payload)
         post_data = simplejson.loads(request.form['payload'])
-        return 'Deploy Website'
+
+        ''' Execute deployment script '''
+        try:
+            #app.logger.debug('Running Script')
+            output = check_output(['bash', '/opt/git_hook/deploy_website.sh'])
+            #app.logger.debug(output)
+
+        except CalledProcessError as e:
+            app.logger.error('[PROCESS_ERROR]: {}'.format(e))
+
+        except:
+            app.logger.error('[OS_ERROR] {}'.format(sys.exc_info()[0]))
+
+        return 'Website Deployed'
     else:
         return 'GET request not supported'
 
 
 if __name__ == '__main__':
-    #app.debug = True
-    #app.host = '0.0.0.0'
+    #app.run(host='0.0.0.0', port=80, debug=True)
     app.run()
